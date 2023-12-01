0891c709bca8d214365440d0e8cf477520cfda65
JosueeeC <josuecastillo_m@hotmail.com>
2023-11-30 21:55:14 -0500

    v1

---
+++ WebApplication1/Controllers/CustomerCustomController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomerCustomController : ControllerBase
+    {
+        private readonly ProjectContext _projectContext;
+        public CustomerCustomController(ProjectContext projectContext) {
+            _projectContext = projectContext;        
+        }
+
+        [HttpPost]
+        public List<Customer> GetByFilter(Customer request)
+        {
+            var response = _projectContext.Customers.Where(x=> x.FirstName.Contains(request.FirstName)  || x.LastName.Contains(request.LastName) ).ToList();
+
+            return response;
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/InvoiceCustomController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceCustomController : ControllerBase
+    {
+        private readonly ProjectContext _projectContext;
+        public InvoiceCustomController(ProjectContext projectContext)
+        {
+            _projectContext = projectContext;
+        }
+
+
+        [HttpPost]
+        public List<Invoice> GetByFilter(string request)
+        {
+            var response = _projectContext.Invoices.Include(x=> x.Customer.FirstName.Contains(request)).ToList();
+
+            return response;
+        }
+    }
+}
